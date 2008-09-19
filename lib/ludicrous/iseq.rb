eafccaa4aba4e49c1546a667a71f36c4d8d0dc2b
Paul Brannan <curlypaul924@gmail.com>
2008-09-19 05:56:36 -0400

    Call env.pc.advance instead of env.advance.

--- lib/ludicrous/iseq.rb
+++ lib/ludicrous/iseq.rb
@@ -2,9 +2,13 @@ class VM
   class InstructionSequence
     def ludicrous_compile(function, env)
       self.each do |instruction|
+        # env.stack.sync_sp
+        # function.debug_inspect_object instruction
         env.make_label
         instruction.ludicrous_compile(function, env)
-        env.advance(instruction.length)
+        env.pc.advance(instruction.length)
+        # env.stack.sync_sp
+        # env.stack.debug_inspect
       end
     end
   end
