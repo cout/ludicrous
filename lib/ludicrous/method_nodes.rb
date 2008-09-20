d215164057e446092935bb8b76232ab657db60ad
Paul Brannan <curlypaul924@gmail.com>
2008-09-20 10:08:41 -0400

    Refactored compilation of SCOPE and METHOD.

--- lib/ludicrous/method_nodes.rb
+++ lib/ludicrous/method_nodes.rb
@@ -85,104 +85,64 @@ def ludicrous_compile_toplevel(
   end
 end
 
-class SCOPE
-  def ludicrous_compile_into_function(
-      origin_class,
-      options = Ludicrous::CompileOptions.new)
-    arg_names = self.argument_names
-    args = self.arguments
+class MethodNodeCompiler
+  attr_reader :node
+  attr_reader :origin_class
+  attr_reader :compile_options
 
-    has_optional_params = false
-    args.each do |name, arg|
-      has_optional_params = true if arg.optional?
+  attr_reader :arg_names
+  attr_reader :args
+  attr_reader :has_optional_params
+
+  def initialize(node, origin_class, compile_options)
+    @node = node
+    @origin_class = origin_class
+    @compile_options = compile_options
+
+    @arg_names = node.argument_names
+    @args = node.arguments
+
+    @has_optional_params = false
+    @args.each do |name, arg|
+      @has_optional_params = true if arg.optional?
     end
+  end
 
-    if not has_optional_params then
-      # all arguments required for this method
-      signature = JIT::Type.create_signature(
-        JIT::ABI::CDECL,
-        JIT::Type::OBJECT,
-        [ JIT::Type::OBJECT ] * (1 + arg_names.size))
-    else
+  def create_arguments_compiler
+    if @has_optional_params then
       # some arguments are optional for this method
-      signature = JIT::Type::RUBY_VARARG_SIGNATURE
+      @arguments_compiler = VariableArgumentsCompiler.new(
+          @arg_names,
+          @args)
+    else
+      # all arguments required for this method
+      @arguments_compiler = FixedArgumentsCompiler.new(
+          @arg_names)
     end
+  end
+
+  def jit_signature
+    return @variable_compiler.jit_signature
+  end
+
+  def create_environment(function)
+    return self.node.ludicrous_create_environment(self, function)
+  end
+
+  def compile
+    arguments_compiler = create_arguments_compiler()
+    signature = arguments_compiler.jit_signature
 
     JIT::Context.build do |context|
       function = JIT::Function.compile(context, signature) do |f|
-        f.optimization_level = options.optimization_level
+        f.optimization_level = @compile_options.optimization_level
 
-        needs_addressable_scope, var_names = self.next.ludicrous_scope_info
-        var_names.concat arg_names
-        var_names.uniq!
-        scope_type = needs_addressable_scope \
-          ? Ludicrous::AddressableScope \
-          : Ludicrous::Scope
-        scope = scope_type.new(f, var_names)
-        env = Ludicrous::Environment.new(
-            f,
-            options,
-            origin_class,
-            scope)
+        env = create_environment(f)
 
         begin
-          if not has_optional_params then
-            # all arguments required for this method
-            env.scope.self = f.get_param(0)
-            arg_names.each_with_index do |arg_name, idx|
-              # TODO: how to deal with default values?
-              env.scope.arg_set(arg_name,  f.get_param(idx+1))
-            end
-          else
-            # some arguments are optional for this method
-            optional_args = args.dup
-            optional_args.delete_if { |name, arg| !arg.optional? }
+          arguments_compiler.compile_assign_arguments(env)
 
-            argc = f.get_param(0)
-            argv = f.get_param(1)
-            env.scope.self = f.get_param(2)
-
-            arg_names.each_with_index do |arg_name, idx|
-              arg = args[arg_name]
-              if arg.required? then
-                # required arg
-                val = f.insn_load_relative(argv, idx*4, JIT::Type::OBJECT)
-                env.scope.arg_set(arg_name, val)
-              elsif arg.rest?
-                var_idx = f.const(JIT::Type::INT, idx)
-                f.if(argc > var_idx) {
-                  rest = f.rb_ary_new4(argc - var_idx, argv + var_idx*f.const(JIT::Type::UINT, 4)) # TODO
-                  env.scope.rest_arg_set(arg_name, rest)
-                } .else {
-                  rest = f.rb_ary_new()
-                  env.scope.rest_arg_set(arg_name, rest)
-                } .end
-              elsif arg.block?
-                raise "Can't handle block arg"
-              else
-                # optional arg
-                val = f.value(JIT::Type::OBJECT)
-                var_idx = f.const(JIT::Type::INT, idx)
-                f.if(var_idx < argc) {
-                  # this arg was passed in
-                  val.store(f.insn_load_relative(argv, idx*4, JIT::Type::OBJECT))
-                } .else {
-                  # this arg was not passed in
-                  val.store(optional_args[arg_name].node_or_iseq_for_default.ludicrous_compile(f, env))
-                } .end
-                env.scope.arg_set(arg_name, val)
-              end
-            end
-          end
-
-          # require 'nodepp'
-          # pp self
-          result = self.ludicrous_compile(f, env)
-          if not result.is_returned then
-            f.insn_return(result)
-          end
-          # puts f.dump
-          # puts "About to compile..."
+          yield(f, env)
         rescue Exception
           if env.file and env.line then
             $!.message << " at #{env.file}:#{env.line}"
@@ -198,51 +158,189 @@ class SCOPE
   end
 end
 
-class METHOD
-  def ludicrous_compile_into_function(
-      origin_class,
-      options = Ludicrous::CompileOptions.new)
-    # p self
-    # p self.argument_names
+class ArgumentsCompiler
+  def initialize(arg_names)
+    @arg_names = arg_names
+  end
+end
 
-    # TODO
-    arg_names = []
+class FixedArgumentsCompiler < ArgumentsCompiler
+  def initialize(arg_names)
+    super(arg_names)
+  end
 
-    signature = JIT::Type.create_signature(
+  def jit_signature
+    return JIT::Type.create_signature(
       JIT::ABI::CDECL,
       JIT::Type::OBJECT,
-      [ JIT::Type::OBJECT ] * (1 + arg_names.size))
+      [ JIT::Type::OBJECT ] * (1 + @arg_names.size))
+  end
 
-    JIT::Context.build do |context|
-      function = JIT::Function.compile(context, signature) do |f|
-        f.optimization_level = options.optimization_level
+  def compile_assign_arguments(env)
+    env.scope.self = env.function.get_param(0)
+    @arg_names.each_with_index do |arg_name, idx|
+      # TODO: how to deal with default values?
+      env.scope.arg_set(arg_name, env.function.get_param(idx+1))
+    end
+  end
+end
 
-        needs_addressable_scope = true # TODO
-        var_names = self.body.local_table
+class VariableArgumentsCompiler < ArgumentsCompiler
+  def initialize(arg_names, args)
+    super(arg_names)
 
-        scope_type = needs_addressable_scope \
-          ? Ludicrous::AddressableScope \
-          : Ludicrous::Scope
-        scope = scope_type.new(f, var_names)
-        env = Ludicrous::YarvEnvironment.new(
-            f,
-            options,
-            origin_class,
-            scope,
-            self.body)
+    @args = args
 
-        # TODO: not always valid
-        env.scope.self = f.get_param(0)
+    @optional_args = @args.dup
+    @optional_args.delete_if { |name, arg| !arg.optional? }
+  end
 
-        # TODO: args
-        # TODO: body
-        # TODO: return value
-        # result = f.const(JIT::Type::INT, Ludicrous::Qnil)
-        # puts self.body.disasm
-        result = self.body.ludicrous_compile(f, env)
+  def jit_signature
+    return JIT::Type::RUBY_VARARG_SIGNATURE
+  end
 
-        # LEAVE instruction should generate return instruction
+  def compile_assign_arguments(env)
+    # TODO: Create a JIT::Array?
+    argc = env.function.get_param(0)
+    argv = env.function.get_param(1)
+    env.scope.self = env.function.get_param(2)
+
+    @arg_names.each_with_index do |arg_name, idx|
+      arg = @args[arg_name]
+      if arg.required? then
+        self.compile_assign_required_argument(env, arg, idx, argc, argv)
+      elsif arg.rest?
+        self.compile_assign_rest_argument(env, arg, idx, argc, argv)
+      elsif arg.block?
+        self.compile_assign_block_argument(env, arg, idx, argc, argv)
+      else
+        self.compile_assign_optional_argument(env, arg, idx, argc, argv)
       end
+    end
+  end
+
+  def compile_assign_required_argument(env, arg, idx, argc, argv)
+    # TODO: 64-bit safe?
+    # (even so, I need to comment why I'm multiplying idx by 4)
+    val = env.function.insn_load_relative(argv, idx * 4, JIT::Type::OBJECT)
+    env.scope.arg_set(arg.name, val)
+  end
+
+  def compile_assign_rest_argument(env, arg, idx, argc, argv)
+    var_idx = env.function.const(JIT::Type::INT, idx)
+    env.function.if(argc > var_idx) {
+      # TODO: not 64-bit safe?
+      rest = env.function.rb_ary_new4(
+          argc - var_idx,
+          argv + var_idx * env.function.const(JIT::Type::UINT, 4))
+      env.scope.rest_arg_set(arg.name, rest)
+    } .else {
+      rest = env.function.rb_ary_new()
+      env.scope.rest_arg_set(arg.name, rest)
+    } .end
+  end
+
+  def compile_assign_block_argument(env, arg, idx, argc, argv)
+    raise "Can't handle block arg"
+  end
+
+  def compile_assign_optional_argument(env, arg, idx, argc, argv)
+    val = env.function.value(JIT::Type::OBJECT)
+    var_idx = env.function.const(JIT::Type::INT, idx)
+
+    env.function.if(var_idx < argc) {
+      # this arg was passed in
+      # TODO: 64-bit safe?
+      # (even so, I need to comment why I'm multiplying idx by 4)
+      val.store(env.function.insn_load_relative(
+          argv,
+          idx * 4,
+          JIT::Type::OBJECT))
+    } .else {
+      # this arg was not passed in
+      arg = @optional_args[arg.name]
+      node = arg.node_or_iseq_for_default
+      val.store(node.ludicrous_compile(env.function, env))
+    } .end
+
+    env.scope.arg_set(arg.name, val)
+  end
+end
+
+class SCOPE
+  def ludicrous_create_scope(compiler, function)
+    needs_addressable_scope, var_names = self.next.ludicrous_scope_info
+    var_names.concat compiler.arg_names
+    var_names.uniq!
+
+    scope_type = needs_addressable_scope \
+      ? Ludicrous::AddressableScope \
+      : Ludicrous::Scope
+    scope = scope_type.new(function, var_names)
+  end
+
+  def ludicrous_create_environment(compiler, function)
+    scope = ludicrous_create_scope(compiler, function)
+    env = Ludicrous::Environment.new(
+        function,
+        compiler.compile_options,
+        compiler.origin_class,
+        scope)
+  end
+
+  def ludicrous_compile_into_function(
+      origin_class,
+      compile_options = Ludicrous::CompileOptions.new)
+
+    compiler = MethodNodeCompiler.new(
+        self,
+        origin_class,
+        compile_options)
+
+    compiler.compile do |function, env|
+      result = self.ludicrous_compile(function, env)
+      if not result.is_returned then
+        function.insn_return(result)
+      end
+    end
+  end
+end
+
+class METHOD
+  def ludicrous_create_scope(compiler, function)
+    needs_addressable_scope = true # TODO
+    var_names = self.body.local_table
+    var_names.concat compiler.arg_names
+    var_names.uniq!
+
+    scope_type = needs_addressable_scope \
+      ? Ludicrous::AddressableScope \
+      : Ludicrous::Scope
+    scope = scope_type.new(function, var_names)
+  end
+
+  def ludicrous_create_environment(compiler, function)
+    scope = ludicrous_create_scope(compiler, function)
+    env = Ludicrous::YarvEnvironment.new(
+        function,
+        compiler.compile_options,
+        compiler.origin_class,
+        scope,
+        self.body)
+  end
+
+  def ludicrous_compile_into_function(
+      origin_class,
+      compile_options = Ludicrous::CompileOptions.new)
+
+    compiler = MethodNodeCompiler.new(
+        self,
+        origin_class,
+        compile_options)
+
+    compiler.compile do |function, env|
+      # LEAVE instruction should generate return instruction
+      self.body.ludicrous_compile(function, env)
     end
   end
 end
