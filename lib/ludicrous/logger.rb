ac3b0ef0afc3a6214ea656ea32a63966e3c878b5
Paul Brannan <cout@sapphire.(none)>
2009-12-05 22:44:05 -0500

    Added documentation.

--- lib/ludicrous/logger.rb
+++ lib/ludicrous/logger.rb
@@ -1,13 +1,17 @@
 module Ludicrous
 
+# A logger that doesn't do any logging (the default logger).
 class NullLogger
   def method_missing(*args)
   end
 end
 
+# By default, use the NullLogger.
 @logger = NullLogger.new
 
 class << self
+  ##
+  # A public accessor for the logger used by the jit compiler.
   attr_accessor :logger
 end
 
