f86319869bae6953d144b688cf428c675cc960f2
Paul Brannan <cout@sapphire.(none)>
2009-12-06 12:45:53 -0500

    Added documentation.

--- lib/ludicrous/module_compiler.rb
+++ lib/ludicrous/module_compiler.rb
@@ -1,3 +1,9 @@
+# Adds methods to the Module class for compiling individual modules or
+# methods in a module.
+
+require 'ludicrous/stubs'
+require 'ludicrous/compile_options'
+
 class Mutex
   ##
   # Never compile the Mutex module
@@ -5,6 +11,20 @@ class Mutex
 end
 
 class Module
+  # Indicates that the given module should be JIT compiled.
+  #
+  # Installs stubs so that:
+  # * Any method in the module will be compiled the first time it is
+  # called
+  # * Any new methods defined after this function is called will have
+  # stubs installed once the method has been defined
+  #
+  # If options.precompile is set, all the methods in the module will be
+  # compiled right away rather than delaying compilation until the
+  # method is called.
+  #
+  # +options+:: a CompileOptions object with parameters indicating how
+  # the methods in this module should be compiled
   def ludicrous_compile(options = Ludicrous::CompileOptions.new)
     return if defined?(@LUDICROUS_FEATURES_APPENDED)
 
@@ -17,12 +37,24 @@ class Module
     include Ludicrous::Speed
   end
 
+  # Allows the user to write:
+  #
+  #   module.go_plaid()
+  #
+  # to compile a module.
   alias_method :go_plaid, :ludicrous_compile
 
+  # Compile the instance method with the given name right away.
+  #
+  # TODO: This method is asymmetric with #ludicrous_compile and is
+  # likely to change in the future.
+  #
+  # +name+ a Symbol indicating the name of the method to compile
   def ludicrous_compile_method(name)
     Ludicrous::Speed.jit_compile_method(self, name)
   end
 
+  # Returns true if this module should not be compiled, false otherwise.
   def ludicrous_dont_compile
     return (self.const_defined?(:LUDICROUS_DONT_COMPILE) or
        (self.const_defined?(:LUDICROUS_OPTIONS) and
