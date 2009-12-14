da03006ebadda90feb499503684e2599503f6f16
Paul Brannan <cout@sapphire.(none)>
2009-12-14 00:51:39 -0500

    Made code more readable.

--- lib/ludicrous/toplevel.rb
+++ lib/ludicrous/toplevel.rb
@@ -15,42 +15,35 @@ def ludicrous_compile_toplevel(
     toplevel_self,
     compile_options = Ludicrous::CompileOptions.new)
 
-  signature = JIT::Type.create_signature(
-    JIT::ABI::CDECL,
-    JIT::Type::OBJECT,
-    [ ])
+  function = JIT::Function.build(context, [ ] => :OBJECT) do |f|
+    f.optimization_level = compile_options.optimization_level
 
-  JIT::Context.build do |context|
-    function = JIT::Function.compile(context, signature) do |f|
-      f.optimization_level = compile_options.optimization_level
+    needs_addressable_scope, vars = self.ludicrous_scope_info
+    vars.uniq!
+    scope_type = needs_addressable_scope \
+      ? Ludicrous::AddressableScope \
+      : Ludicrous::Scope
+    scope = scope_type.new(f, vars)
 
-      needs_addressable_scope, vars = self.ludicrous_scope_info
-      vars.uniq!
-      scope_type = needs_addressable_scope \
-        ? Ludicrous::AddressableScope \
-        : Ludicrous::Scope
-      scope = scope_type.new(f, vars)
+    origin_class = f.const(:OBJECT, toplevel_self.class) # TODO: is this right?
 
-      origin_class = f.const(JIT::Type::OBJECT, toplevel_self.class) # TODO: is this right?
+    env = Ludicrous::Environment.new(
+        f,
+        compile_options,
+        origin_class,
+        scope)
 
-      env = Ludicrous::Environment.new(
-          f,
-          compile_options,
-          origin_class,
-          scope)
+    env.scope.self = f.const(:OBJECT, toplevel_self)
 
-      env.scope.self = f.const(JIT::Type::OBJECT, toplevel_self)
-
-      result = self.ludicrous_compile(f, env)
-      if not result then
-        f.insn_return(f.const(JIT::Type::OBJECT, nil))
-      elsif not result.is_returned then
-        f.insn_return(result)
-      end
+    result = self.ludicrous_compile(f, env)
+    if not result then
+      f.insn_return(f.const(:OBJECT, nil))
+    elsif not result.is_returned then
+      f.insn_return(result)
     end
-
-    return function
   end
+
+  return function
 end
 
 end
